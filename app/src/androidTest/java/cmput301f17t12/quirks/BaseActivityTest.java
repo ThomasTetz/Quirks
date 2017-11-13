cba2793fa76a5752921adff8deecc04b51aa956a
charleshoang1 <choang1@ualberta.ca>
2017-11-13 11:39:12 -0700

    working addeventtesting

--- app/src/androidTest/java/cmput301f17t12/quirks/BaseActivityTest.java
+++ app/src/androidTest/java/cmput301f17t12/quirks/BaseActivityTest.java
@@ -12,12 +12,14 @@ import android.support.design.internal.NavigationMenuView;
 import android.support.design.widget.NavigationView;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
 import android.support.v7.app.ActionBar;
 import android.view.Menu;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import cmput301f17t12.quirks.Activities.BaseActivity;
 import cmput301f17t12.quirks.Activities.LoginActivity;
@@ -43,6 +45,8 @@ import static org.hamcrest.Matchers.allOf;
  * Created by charleshoang on 2017-11-13.
  */
 
+
+@RunWith(AndroidJUnit4.class)
 public class BaseActivityTest {
 
     @Rule
