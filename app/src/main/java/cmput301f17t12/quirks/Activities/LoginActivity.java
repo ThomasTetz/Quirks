c728a0676021be0b22ce768b0db5311a80e72d0b
charleshoang1 <choang1@ualberta.ca>
2017-11-13 03:11:08 -0700

    Clean up code

--- app/src/main/java/cmput301f17t12/quirks/Activities/LoginActivity.java
+++ app/src/main/java/cmput301f17t12/quirks/Activities/LoginActivity.java
@@ -6,16 +6,13 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
