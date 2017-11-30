3e6ebfb35afc6fffc7248b968a7542c8dfd2d5a8
ahnguyen03 <ahnguyen@ualberta.ca>
2017-11-29 18:08:08 -0700

    added Traded tab and need to add friends and request

---
+++ app/src/main/java/cmput301f17t12/quirks/Activities/FriendActivity.java
@@ -0,0 +1,26 @@
+package cmput301f17t12.quirks.Activities;
+
+import android.os.Bundle;
+
+import cmput301f17t12.quirks.R;
+
+/**
+ * Created by root on 11/29/17.
+ */
+
+public class FriendActivity extends SocialActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+        @Override
+    int getContentViewId() {
+        return R.layout.activity_friends;
+    }
+
+    @Override
+    int getNavigationMenuItemId() {
+        return R.id.action_friends;
+    }
+}
--- app/src/main/java/cmput301f17t12/quirks/Activities/SocialActivity.java
+++ app/src/main/java/cmput301f17t12/quirks/Activities/SocialActivity.java
@@ -1,22 +1,77 @@
 package cmput301f17t12.quirks.Activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import cmput301f17t12.quirks.Helpers.BottomNavigationViewHelper;
 import cmput301f17t12.quirks.R;
 
 /**
  * Created by root on 11/29/17.
  */
 
-public class SocialActivity extends BaseActivity {
+
+public  class SocialActivity extends BaseActivity {
+    protected BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //setContentView(getContentViewId());
 
+        navigationView = (BottomNavigationView) findViewById(R.id.top_navigation);
+        navigationView.setOnNavigationItemSelectedListener(this);
+        BottomNavigationViewHelper.removeShiftMode(navigationView);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        updateNavigationBarState();
+    }
+
+    // Remove inter-activity transition to avoid screen tossing on tapping bottom navigation items
+    @Override
+    public void onPause() {
+        super.onPause();
+        overridePendingTransition(0, 0);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.action_friends:
+                startActivity(new Intent(this, FriendActivity.class));
+                break;
+
+        }
+        finish();
+        return true;
+    }
+
+    private void updateNavigationBarState(){
+        int actionId = getNavigationMenuItemId();
+        selectBottomNavigationBarItem(actionId);
+    }
+
+    void selectBottomNavigationBarItem(int itemId) {
+        Menu menu = navigationView.getMenu();
+        for (int i = 0, size = menu.size(); i < size; i++) {
+            MenuItem item = menu.getItem(i);
+            boolean shouldBeChecked = item.getItemId() == itemId;
+            if (shouldBeChecked) {
+                item.setChecked(true);
+                break;
+            }
+        }
     }
 
     @Override
