324e1f9c31924ea016bfdff03af73471bf519cc6
Jordan Lane <jlane@ualberta.ca>
2017-12-01 14:38:07 -0700

    Google Maps now works with Nav Bar

--- app/src/main/java/cmput301f17t12/quirks/Activities/MapActivity.java
+++ app/src/main/java/cmput301f17t12/quirks/Activities/MapActivity.java
@@ -7,9 +7,13 @@ import android.support.design.widget.BottomNavigationView;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnCameraIdleListener;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMapLongClickListener;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -19,87 +23,43 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import cmput301f17t12.quirks.Helpers.BottomNavigationViewHelper;
 import cmput301f17t12.quirks.R;
 
-public class MapActivity extends AppCompatActivity
-        implements OnMapReadyCallback, BottomNavigationView.OnNavigationItemSelectedListener {
+import static cmput301f17t12.quirks.R.id.map;
 
-    /* Currently I have been able to get either:
-        JUST THE MAP FRAGMENT WORKING (within the framelayout)
-        OR JUST THE NAVIGATION BAR
-        -- They don't like to work together
-     */
+public class MapActivity extends BaseActivity
+        implements OnMapReadyCallback, OnMapClickListener, OnMapLongClickListener, OnCameraIdleListener {
 
+    private TextView mTapTextView;
+    private GoogleMap mMap;
     protected BottomNavigationView navigationView;
 
     // [MAP]
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
 
-        navigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        navigationView.setOnNavigationItemSelectedListener(this);
-        BottomNavigationViewHelper.removeShiftMode(navigationView);
+        mTapTextView = (TextView) findViewById(R.id.tap_text);
 
         SupportMapFragment mapFragment =
-                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(map);
         mapFragment.getMapAsync(this);
 
     }
-
-    /* Non Map - Core basic testing
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-    }*/
-
 
     // [MAP]
     @Override
     public void onMapReady(GoogleMap googleMap) {
         // Add a marker in Sydney, Australia,
         // and move the map's camera to the same location.
+        mMap = googleMap;
+
+        mMap.setOnMapClickListener(this);
+        mMap.setOnMapLongClickListener(this);
+        mMap.setOnCameraIdleListener(this);
+
         LatLng sydney = new LatLng(-33.852, 151.211);
         googleMap.addMarker(new MarkerOptions().position(sydney)
                 .title("Marker in Sydney"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        updateNavigationBarState();
-    }
-
-    // Remove inter-activity transition to avoid screen tossing on tapping bottom navigation items
-    @Override
-    public void onPause() {
-        super.onPause();
-        overridePendingTransition(0, 0);
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_geomap:
-                startActivity(new Intent(this, MapActivity.class));
-                break;
-            case R.id.action_social:
-                // TODO: Part 5
-//                startActivity(new Intent(this, SocialActivity.class));
-//                break;
-            case R.id.action_newevent:
-                startActivity(new Intent(this, NewEventActivity.class));
-                break;
-            case R.id.action_quirklist:
-                startActivity(new Intent(this, QuirksActivity.class));
-                break;
-            case R.id.action_home:
-                startActivity(new Intent(this, MainActivity.class));
-                break;
-        }
-        finish();
-        return true;
     }
 
     // [Navigation Bar]
@@ -112,21 +72,16 @@ public class MapActivity extends AppCompatActivity
         return R.id.action_geomap;
     }
 
-    private void updateNavigationBarState(){
-        int actionId = getNavigationMenuItemId();
-        selectBottomNavigationBarItem(actionId);
+
+    @Override
+    public void onCameraIdle() { mTapTextView.setText(mMap.getCameraPosition().toString());}
+
+    @Override
+    public void onMapClick(LatLng point) {
+        mTapTextView.setText("tapped, point=" + point);
     }
 
-    void selectBottomNavigationBarItem(int itemId) {
-        Menu menu = navigationView.getMenu();
-        for (int i = 0, size = menu.size(); i < size; i++) {
-            MenuItem item = menu.getItem(i);
-            boolean shouldBeChecked = item.getItemId() == itemId;
-            if (shouldBeChecked) {
-                item.setChecked(true);
-                break;
-            }
-        }
-    }
 
+    @Override
+    public void onMapLongClick(LatLng point) { mTapTextView.setText("long pressed, point=" + point);}
 }
