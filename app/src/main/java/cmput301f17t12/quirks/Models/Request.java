dbe46933e3d38b41e5b5900f643c465a42546ff8
ahnguyen03 <ahnguyen@ualberta.ca>
2017-12-01 16:20:37 -0700

    added abstract request model and UserRequest model

--- app/src/main/java/cmput301f17t12/quirks/Models/Request.java
+++ app/src/main/java/cmput301f17t12/quirks/Models/Request.java
@@ -6,19 +6,15 @@ package cmput301f17t12.quirks.Models;
 
 public abstract class Request {
    User fromUser;
-   User toUser;
 
-   public Request(User fromUser, User toUser){
+   public Request(User fromUser){
        this.fromUser = fromUser;
-       this.toUser = toUser;
    }
 
    public User getFromUser(){
        return this.fromUser;
    }
-   public  User getToUser(){
-       return this.toUser;
-   }
+
 
    abstract String getDetails();
 }
--- app/src/main/java/cmput301f17t12/quirks/Models/RequestList.java
+++
@@ -1,8 +0,0 @@
-package cmput301f17t12.quirks.Models;
-
-/**
- * Created by root on 12/1/17.
- */
-
-public class RequestList {
-}
