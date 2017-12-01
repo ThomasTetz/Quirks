ded194334920beb924cda13090acb08bf39b9e06
ahnguyen03 <ahnguyen@ualberta.ca>
2017-11-30 20:24:53 -0700

    custom adapter for findfriends need to implement filtering through db by username to get users

--- app/src/main/java/cmput301f17t12/quirks/Activities/FindFriendActivity.java
+++ app/src/main/java/cmput301f17t12/quirks/Activities/FindFriendActivity.java
@@ -4,7 +4,19 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import cmput301f17t12.quirks.Adapters.FindFriendListItemAdapter;
+import cmput301f17t12.quirks.Adapters.FriendListItemAdapter;
+import cmput301f17t12.quirks.Helpers.HelperFunctions;
+import cmput301f17t12.quirks.Models.Inventory;
+import cmput301f17t12.quirks.Models.QuirkList;
 import cmput301f17t12.quirks.Models.User;
 import cmput301f17t12.quirks.R;
 
@@ -14,11 +26,17 @@ import cmput301f17t12.quirks.R;
 
 public class FindFriendActivity extends SocialActivity {
     public User currentlylogged;
+    private FindFriendListItemAdapter adapter;
+    private ArrayList<User> userList;
+    EditText searchFriendUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        userList = new ArrayList<>();
+        Button addFriendButton = (Button) findViewById(R.id.buttonAddFriend);
+        searchFriendUser = (EditText)findViewById(R.id.editTextFindFriend);
+        ImageButton searchFriendButton = (ImageButton) findViewById(R.id.imageButtonSearch);
         SharedPreferences settings = getSharedPreferences("dbSettings", Context.MODE_PRIVATE);
         String jestID = settings.getString("jestID", "defaultvalue");
 
@@ -27,9 +45,45 @@ public class FindFriendActivity extends SocialActivity {
         }
 
 
+        Inventory dummyInv = new Inventory();
+        ArrayList<User> friends = new ArrayList<>();
+        QuirkList quirks = new QuirkList();
+
+        User dummy = new User("dummy",dummyInv,friends,quirks);
+        User dummy2 = new User("dummy2",dummyInv,friends,quirks);
+        User dummy3 = new User("Alex",dummyInv,friends,quirks);
+
+        currentlylogged = HelperFunctions.getUserObject(jestID);
+
+        userList.add(dummy);
+        userList.add(dummy2);
+        userList.add(dummy3);
+
+        ListView lView = (ListView)findViewById(R.id.findfriend_listview);
+        adapter = new FindFriendListItemAdapter(userList,this);
+        lView.setAdapter(adapter);
+
+
 
     }
 
+    public void findFriendSearchBut(View view){
+        String query = "";
+        query = getQueryFilterUser();
+        String searchFriendsUser = searchFriendUser.getText().toString();
+    }
+
+    public void addFriendsBut(View view){
+
+    }
+
+    public String getQueryFilterUser(){
+        String query = "user";
+        // for the user
+        // look in quirklist
+        // match where type is type
+        return query;
+    }
 
     @Override
     int getContentViewId() {
---
+++ app/src/main/java/cmput301f17t12/quirks/Adapters/FindFriendListItemAdapter.java
@@ -0,0 +1,65 @@
+package cmput301f17t12.quirks.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import cmput301f17t12.quirks.Models.User;
+import cmput301f17t12.quirks.R;
+
+/**
+ * Created by root on 11/30/17.
+ */
+
+public class FindFriendListItemAdapter extends BaseAdapter implements ListAdapter {
+
+    ArrayList<User> users = new ArrayList<>();
+    private Context context;
+
+    public FindFriendListItemAdapter(ArrayList<User> users, Context context){
+        this.users = users;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return users.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return users.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        View view = convertView;
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.findfriend_item, null);
+
+            User user = users.get(i);
+            TextView friendHeader = (TextView) view.findViewById(R.id.FindFriendtext);
+            friendHeader.setText(user.getUsername());
+
+            CheckBox checkBoxfriends = (CheckBox) view.findViewById(R.id.checkBoxSelectFriend);
+
+
+        }
+
+        return view;
+    }
+}
