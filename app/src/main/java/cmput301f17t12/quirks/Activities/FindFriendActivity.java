9eef8fde6b6b045a9d16ab51e7266cc499a54e17
ahnguyen03 <ahnguyen@ualberta.ca>
2017-12-01 17:39:59 -0700

    in Findfriends the button is now working just needs to implement request passing between the users

--- app/src/main/java/cmput301f17t12/quirks/Activities/FindFriendActivity.java
+++ app/src/main/java/cmput301f17t12/quirks/Activities/FindFriendActivity.java
@@ -126,12 +126,14 @@ public class FindFriendActivity extends SocialActivity {
         return query;
     }
 
-    public void addFriend(View view){
-        Toast.makeText(FindFriendActivity.this,"Sending Friend Request", Toast.LENGTH_SHORT).show();
-
+    public void addFriend(int selectFriendIndex) {
+        Toast.makeText(FindFriendActivity.this,"Sending Friend Request",Toast.LENGTH_SHORT).show();
+        String username = userList.get(selectFriendIndex).getUsername();
+        Log.d(TAG, "addFriend: the index is this " + selectFriendIndex);
+        Log.d(TAG, "addFriend: the index is this " + username);
     }
 
-    @Override
+        @Override
     int getContentViewId() {
         return R.layout.activity_findfriends;
     }
